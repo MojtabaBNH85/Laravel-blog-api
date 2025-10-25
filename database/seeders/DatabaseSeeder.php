6f85726e5b67a2c7632da229f4f4a53a7f84c766
MojtabaBNH85 <bnhmojtaba@gmail.com>
2025-10-25 11:00:10 +0330

    feat(seed): add factory and seeder for users, posts, comments and reactions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +20,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        $users = User::factory(20)->create();
+
+        $posts = Post::factory(15)->create([
+            'user_id' => $users->random()->id
         ]);
+
+        $comments = Comment::factory(15)->create([
+            'post_id' => $posts->random()->id,
+            'user_id' => $users->random()->id
+        ]);
+
+
+        foreach ($posts as $post) {
+            foreach ($users->random(rand(0,10)) as $user) {
+                DB::table('post_user_reactions')->insert([
+                    'post_id' => $post->id,
+                    'user_id' => $user->id,
+                    'reaction' => fake()->randomElement(['like' , 'dislike']),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
